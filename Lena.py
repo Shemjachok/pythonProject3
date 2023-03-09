7f40591bf2bd9d6340e03d06531f03a651805e77
Shemjachok <shmjak@yandex.ru>
2023-03-09 12:40:50 +0300

    Lena

--- Lena.py
+++ Lena.py
@@ -2433,28 +2433,58 @@ import math
 # bot.polling(none_stop=True)
 
 
-import requests  # импортируем наш знакомый модуль
-import lxml.html
-from lxml import etree
-
+# import requests  # импортируем наш знакомый модуль
+# import lxml.html
+# from lxml import etree
+#
 # html = requests.get('https://www.python.org/').content  # получим html главной странички официального сайта Python
 #
-# tree = lxml.html.document_fromstring(html)
-# title = tree.xpath('/html/head/title/text()')  # забираем текст тега <title> из тега <head> который лежит в свою очередь внутри тега <html> (в этом невидимом теге <head> у нас хранится основная информация о страничке. Её название и инструкции по отображению в браузере.
+# # tree = lxml.html.document_fromstring(html)
+# # title = tree.xpath('/html/head/title/text()')  # забираем текст тега <title> из тега <head> который лежит в свою очередь внутри тега <html> (в этом невидимом теге <head> у нас хранится основная информация о страничке. Её название и инструкции по отображению в браузере.
+# #
+# # print(title)  # выводим полученный заголовок страницы
 #
-# print(title)  # выводим полученный заголовок страницы
+#
+#
+# # from lxml import etree
+#
+# # создадим объект ElementTree. Он возвращается функцией parse()
+# tree = etree.parse('Welcome to Python.org.html', lxml.html.HTMLParser())  # попытаемся спарсить наш файл с помощью HTML-парсера. Сам HTML — это то, что мы скачали и поместили в папку из браузера.
+#
+# ul = tree.findall('/body/div/div[3]/div/section/div[2]/div[1]/div/ul/li')  # помещаем в аргумент методу findall скопированный xpath. Здесь мы получим все элементы списка новостей. (Все заголовки и их даты)
+#
+# # создаём цикл? в котором будем выводить название каждого элемента из списка
+# for li in ul:
+#     a = li.find('a')  # в каждом элементе находим, где хранится заголовок новости. У нас это тег <a>. Т.е. гиперссылка, на которую нужно нажать, чтобы перейти на страницу с новостью. Гиперссылки в HTML — это всегда тэг <a>.
+#     time = li.find('time')
+#     print(time.get('datetime'), a.text)  # из этого тега забираем текст - это и будет нашим названием
 
 
+# import lxml.html
+#
+# html = ''' <html>
+#  <head> <title> Some title </title> </head>
+#  <body>
+#   <tag1> some text
+#      <tag2> MY TEXT </tag2>
+#    </tag1>
+#  </body>
+# </html>
+# '''
+#
+# tree = lxml.html.document_fromstring(html)
+#
+# text = tree.xpath('/html/body/tag1/tag2/text()')
+#
+# print(text)
 
-from lxml import etree
 
-# создадим объект ElementTree. Он возвращается функцией parse()
-tree = etree.parse('Welcome to Python.org.html', lxml.html.HTMLParser())  # попытаемся спарсить наш файл с помощью HTML-парсера. Сам HTML — это то, что мы скачали и поместили в папку из браузера.
+import redis
 
-ul = tree.findall('body/div /div[3]/ section / div[2] / div[1] / div / ul / li')  # помещаем в аргумент методу findall скопированный xpath. Здесь мы получим все элементы списка новостей. (Все заголовки и их даты)
-
-# создаём цикл? в котором будем выводить название каждого элемента из списка
-for li in ul:
-    a = li.find('a')  # в каждом элементе находим, где хранится заголовок новости. У нас это тег <a>. Т.е. гиперссылка, на которую нужно нажать, чтобы перейти на страницу с новостью. Гиперссылки в HTML — это всегда тэг <a>.
-    print(a.text)  # из этого тега забираем текст — это и будет нашим названием
-
+red = redis.Redis(
+    host=redis-16450.c280.us-central1-2.gce.cloud.redislabs.com, # ваш хост, если вы поставили Redis к себе на локальную машину, то у вас это будет localhost. Если же вы находитесь на Windows, то воспользуйтесь полем host из вашей облачной БД, которую мы создавали в скринкасте.
+    port=16450, # порт подключения. На локальной машине это должно быть 6379. Для пользователей облачного сервиса порт всегд разный, по этому его надо копировать оттуда же, что и host.
+    password=5Rz9VSJE9ooU9uKB0JTroNr69w1SRIWA # для локальной машины пароль не требуется (если вы устанавливали Redis к себе на компьютер и не пользовались облачным сервисом из скринкаста выше). Для пользователей облачного сервиса пароль находится в вашей облачной базе данных в поле password.
+)
+red.set('var1', 'value1') # записываем в кеш строку "value1"
+print(red.get('var1'))
